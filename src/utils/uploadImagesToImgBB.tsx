3ebfb7c86ff843c35b8c12fba692cc4f8e4570df
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-12-31 12:24:02 +0600

    image hosting change

--- src/utils/uploadImagesToImgBB.tsx
+++ src/utils/uploadImagesToImgBB.tsx
@@ -1,37 +1,33 @@
-import envConfig from "@/src/config/envConfig";
+import envConfig from "../config/envConfig";
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export const uploadImagesToImgBB = async (files: any) => {
-  // const apiKey = envConfig?.imageBbApiKey; // Your API key
-  const apiKey = "7ef1a84c76b5db81332f5d3ab765a5e3"; // Your API key
-  const expiration = 600; // Image expiration time in seconds
-  const uploadUrl = `https://api.imgbb.com/1/upload?expiration=${expiration}&key=${apiKey}`;
-
-  const promises = Array.from(files).map((file: any) => {
-    const formData = new FormData();
-    formData.append("image", file); // Convert the image file to base64 string
-
-    return fetch(uploadUrl, {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.success) {
-          return result.data.url; // Return the URL of the uploaded image
-        } else {
-          throw new Error("Upload failed");
-        }
-      })
-      .catch((error) => {
-        console.error("Error uploading image:", error);
-        return null;
-      });
-  });
-
   try {
-    const uploadedUrls = await Promise.all(promises);
-    return uploadedUrls.filter((url) => url !== null); // Return valid URLs only
+    // Create an array of promises for each file upload
+    const uploadPromises = files.map((file: any) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", envConfig.cloudPreset as string);
+
+      return fetch(
+        `https://api.cloudinary.com/v1_1/${envConfig.cloudName}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to upload image to Cloudinary.");
+          }
+          return response.json();
+        })
+        .then((data) => data.secure_url);
+    });
+
+    // Wait for all uploads to complete
+    const secureUrls = await Promise.all(uploadPromises);
+
+    return secureUrls;
   } catch (error) {
     console.error("Error uploading images:", error);
     throw error;
